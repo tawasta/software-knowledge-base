3393291ae6418db8f4b7eb6d484143230deb76b5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-05-08 11:34:02 +0300

    Remove team Add vcs_team

--- repository.py
+++ repository.py
@@ -12,12 +12,15 @@ class Repository(models.Model):
     ''' Columns '''
     name = fields.Char('Name', help='E.g. "odoo-customizations" or "moodle-extension"')
     description = fields.Char('Description', help='Full name or description')
-    team = fields.Char('Team', help='E.g. "companyname" or "teamname"')
     url = fields.Char('URL', help='The full URL')
     vcs = fields.Many2one('software_knowledge_base.vcs', string='Type', default='git')
     vcs_host = fields.Many2one('software_knowledge_base.vcs_host', string='Host', default='Github')
+    vcs_team = fields.Many2one('software_knowledge_base.vcs_team', string='Team')
         
     @api.one
     @api.onchange('name', 'team', 'vcs', 'vcs_host')
     def generate_url(self):
-        self.url = "%s/%s/%s" % (self.vcs_host.address or '', self.team or '' , self.name or '')
+        self.url = "%s/%s/%s" % (self.vcs_host.address or '', self.vcs_team.name or '' , self.name or '')
+        
+        ''' TODO: validate url structure '''
+        ''' TODO: check if url exists '''
