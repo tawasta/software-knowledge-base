353c6cb22bd491251cfbc3ca67f821b96fd66c56
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-05-08 10:09:03 +0300

    Add VCS host model

---
+++ vcs_host.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api, _
+from openerp.tools.translate import _
+
+class VcsHost(models.Model):
+    
+    _name = 'software_knowledge_base.vcs_host'
+    _description = 'Version Control System Host'
+    
+    ''' Columns '''
+    name = fields.Char('Name', help='E.g. github or bitbucket')
+    description = fields.Text('Description', help='Longer description, if needed')
+    url = fields.Char('URL', help='E.g. https://github.com or https://bitbucket.org')
