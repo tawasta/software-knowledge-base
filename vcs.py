a5356379a17bf349987669ac77a49cb5c24ad28e
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-05-08 11:19:50 +0300

    Add a default order

--- vcs.py
+++ vcs.py
@@ -6,7 +6,14 @@ class Vcs(models.Model):
     
     _name = 'software_knowledge_base.vcs'
     _description = 'Version Control System'
+    _order = 'name'
     
     ''' Columns '''
     name = fields.Char('Name', help='E.g. git or svn')
     description = fields.Text('Description', help='The full name')
+    display_name = fields.Char(string='Name', compute='_compute_display_name')
+
+    @api.one
+    @api.depends('name', 'description')
+    def _compute_display_name(self):
+        self.display_name = "%s (%s)" % (self.name, self.description)
