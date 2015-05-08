248b3e58b34a828344ef125632e70d3721180875
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-05-08 11:19:12 +0300

    Update descriptions Add onchange method

--- repository.py
+++ repository.py
@@ -10,8 +10,14 @@ class Repository(models.Model):
     _order = 'name'
     
     ''' Columns '''
-    name = fields.Char('Name', help='E.g. "Odoo customizations" or "Moodle extension"')
-    description = fields.Text('Description', help='A longer description')
-    type = fields.Char('Repository type', help='E.g. "git" or "svn"')
-    team = fields.Char('Repository team', help='E.g. "companyname" or "teamname"')
-    host = fields.Char('Repository host', help='E.g. "github" or "bitbucket"')
+    name = fields.Char('Name', help='E.g. "odoo-customizations" or "moodle-extension"')
+    description = fields.Char('Description', help='Full name or description')
+    team = fields.Char('Team', help='E.g. "companyname" or "teamname"')
+    url = fields.Char('URL', help='The full URL')
+    vcs = fields.Many2one('software_knowledge_base.vcs', string='Type', default='git')
+    vcs_host = fields.Many2one('software_knowledge_base.vcs_host', string='Host', default='Github')
+        
+    @api.one
+    @api.onchange('name', 'team', 'vcs', 'vcs_host')
+    def generate_url(self):
+        self.url = "%s/%s/%s" % (self.vcs_host.address or '', self.team or '' , self.name or '')
