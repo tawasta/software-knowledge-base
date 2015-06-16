8b83e2d142503ac1400715440dda2d259bd63d39
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-06-16 12:07:26 +0300

    Add address readme for data insert, though it's not used anywhere

--- vcs_host.py
+++ vcs_host.py
@@ -12,7 +12,7 @@ class VcsHost(models.Model):
     name = fields.Char('Name', help='E.g. github or bitbucket')
     description = fields.Text('Description', help='Longer description, if needed')
     address = fields.Char('Address', help='E.g. https://github.com or 192.168.100.100')
-    #address_readme = fields.Char('Readme Address', help='E.g. https://raw.githubusercontent.com')
+    address_readme = fields.Char('Readme Address', help='E.g. https://raw.githubusercontent.com')
     #provider = fields.Many2one('software_knowledge_base.vcs_provider', string='Provider')
     readme_autofetch = fields.Boolean("Automatically fetch README files")
     
