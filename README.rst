f35defd92ae78d680e22a03701e18cf97acd5f49
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-01-27 15:12:18 +0200

    Add readme and update descriptor

---
+++ README.rst
@@ -0,0 +1,68 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===================================
+Software development knowledge base
+===================================
+
+A module for maintaining data regarding software installations and modules. Built mainly around the needs
+of a Odoo development company, but if you do module-based software development and offer cloud services,
+you may find this it useful for other platforms as well.
+
+Features
+--------
+ * Adds the following models for storing and organizing development-related data:
+  * server
+  * server note
+  * installation
+  * platform
+  * module
+  * external component
+  * repository
+  * repository tags
+  * vcs
+  * vcs host
+ * Links installations to projects and partners
+
+Access rights
+-------------
+ * By default, all Odoo users have read access to knowledge base items.
+ * Creates two new groups: Developer and Administrator
+ * Administrators have full create/write/unlink access to all knowledge base items
+ * Developers have otherwise full access but they cannot create or unlink installations, servers or platforms.
+
+Installation
+============
+
+Install the module form Settings->Local Modules
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Firstname Lastname <email.address@tawasta.fi>
+* Second Person <second.person@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
--- __openerp__.py
+++ __openerp__.py
@@ -1,75 +1,66 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2014- Vizucom Oy (http://www.vizucom.com)
+#    Author: Vizucom Oy
+#    Copyright 2016 Vizucom Oy (http://www.vizucom.com)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
+
 {
     'name': 'Software development knowledge base',
+    'summary': 'A module for maintaining data regarding software installations and modules',
+    'version': '8.0.1.0.3',
     'category': 'Knowledge',
-    'version': '8.0.1.0.2',
+    'website': 'http://www.vizucom.com',
     'author': 'Vizucom Oy',
-    'website': 'http://www.vizucom.com',    
-    'depends': ['project'],
-    'description': """
-SW Development knowledge base
-=============================
-A module for maintaining data regarding software installations and modules. Built mainly around the needs
-of a Odoo development company, but if you do module-based software development and offer cloud services,
-you may find this it useful for other platforms as well.
-
-Features
---------
- * Adds the following models for storing and organizing development-related data:
-  * server
-  * server note
-  * installation
-  * platform
-  * module
-  * external component
-  * repository
-  * repository tags
-  * vcs
-  * vcs host
- * Links installations to projects and partners 
-
-Access rights
--------------
- * By default, all Odoo users have read access to knowledge base items.
- * Creates two new groups: Developer and Administrator
- * Administrators have full create/write/unlink access to all knowledge base items
- * Developers have otherwise full access but they cannot create or unlink installations, servers or platforms.
-
-""",
+    'license': 'AGPL-3',
+    'application': True,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'project',
+    ],
     'data': [
-        'views/installation.xml',
-        'views/module.xml',
-        'views/server.xml',
-        'views/server_note.xml',
-        'views/external_component.xml',
-        'views/platform.xml',
-        'views/repository.xml',
-        'views/vcs.xml',
-        #'views/vcs_provider.xml',
-        'views/vcs_host.xml',
-        'views/vcs_team.xml',
-        'views/project_project.xml',
-        'views/project_task.xml',
-        'views/res_partner.xml',
+        'data/installation_data_copy.xml',
         'data/ir_module_category.xml',
         'data/res_groups.xml',
-        'security/ir.model.access.csv',
-        'views/menus.xml',
-        'data/installation_data_copy.xml',
         'data/vcs_data.xml',
         'data/vcs_host_data.xml',
+
+        'security/ir.model.access.csv',
+
+        'views/external_component.xml',
+        'views/installation.xml',
+        'views/menus.xml',
+        'views/module.xml',
+        'views/platform.xml',
+        'views/project_project.xml',
+        'views/project_task.xml',
+        'views/repository.xml',
+        'views/res_partner.xml',
+        'views/server_note.xml',
+        'views/server.xml',
+        'views/vcs_host.xml',
+        'views/vcs_team.xml',
+        'views/vcs.xml',
+        # 'views/vcs_provider.xml',
+    ],
+    'demo': [
     ],
 }
