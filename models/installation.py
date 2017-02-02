6cd68ed5a0f48eb87ac17473ebb49e3804a893da
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-02-02 12:44:24 +0200

    Merge branch '8.0' of ssh://git.intra.fi:10022/odoo/software_knowledge_base into 8.0
    # Conflicts:
    #	__init__.py
    #	__openerp__.py
    #	models/installation.py
    #	models/server.py

--- models/installation.py
+++ models/installation.py
@@ -1,88 +1,95 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv, fields
-from openerp.tools.translate import _
 
-import logging
-_logger = logging.getLogger(__name__)
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
 
 
-class installation(osv.Model):
-    
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class Installation(models.Model):
+    # 1. Private attributes
     _name = 'software_knowledge_base.installation'
-    _description = 'Installation'  
-    _inherit = ['mail.thread']    
+    _description = 'Installation'
+    _inherit = ['mail.thread']
     _order = 'name'
-    
-    _INSTALLATION_STATE_VALUES = [('setup','In setup'),
-                                  ('ready','In use'),
-                                  ('terminated','Terminated')]
-    
 
-    def _copy_inst_data_to_m2m_field(self, cr, uid, ids=None, context=None):
-        ''' In 2/2015 a change was added that enables an installation to be linked to many
-        partners. This code is run on module install/update, it copies the data in the m2o
-        field to the new m2m field, thus preserving the relationships that were done prior to the
-        update. '''
+    _INSTALLATION_STATE_VALUES = [
+        ('setup', 'In setup'),
+        ('ready', 'In use'),
+        ('terminated', 'Terminated')
+    ]
 
-        
-        _logger.info('Starting to update installations data format')
-        installations_with_unmoved_data = self.search(cr, uid, args=[('data_copied_to_m2m','=',False)], context=context)
-        
-        _logger.info('Found %i installations that use the old many2one relation. Starting to update...' % len(installations_with_unmoved_data))        
-        for inst in self.browse(cr, uid, installations_with_unmoved_data):
-            inst_vals = {
-                'data_copied_to_m2m': True            
-            }
-            
-            if inst.partner_id:
-                _logger.info('Installation %s had a matching partner %s. Add a reference to the m2m field...' % (inst.name, inst.partner_id.name) )
-                inst_vals['partner_ids'] = [(4, inst.partner_id.id)]
-            
-            self.write(cr, uid, [inst.id], inst_vals, context=context)
+    # 2. Fields declaration
+    name = fields.Char('Name')
+    company_id = fields.Many2one('res.company', 'Company')
 
-                    
-        return True
-        
-    def copy(self, cr, uid, id, default=None, context=None):
-        ''' Set default data_copied_to_m2m to allow copying installations without being admin '''
-        default['data_copied_to_m2m'] = True
-        
-        return super(installation, self).copy(cr, uid, id, default, context=context)
-        
-    _columns = {
-        'name':             fields.char('Name'),
-        'company_id': fields.many2one('res.company', 'Company'),
-        
-        'state':            fields.selection(_INSTALLATION_STATE_VALUES, 'Status', help='Where the module has been developed'),
-        'additional_info':  fields.text('Additional info'),
-        
-        'platform_id':      fields.many2one('software_knowledge_base.platform', string='Platform'),
-        'server_id':        fields.many2one('software_knowledge_base.server', string='Server'),
-        'port':             fields.integer('Port'),
-        'db_server_id':     fields.many2one('software_knowledge_base.server', string='Database server'),
-        'disk_usage':       fields.float('Disk Usage (MB)'),
-        'url':              fields.char('URL'),
-        'identifier':       fields.char('Identifier'),
-        
-        'project_ids':     fields.many2many('project.project', 'installation_project_rel', 'installation_id', 'project_id',
-                                                 string='Projects', help='Projects utilizing this installation.'),
-                        
-        'partner_id':       fields.many2one('res.partner', string='Customer'), # Legacy field from where there was just 1 partner linked to inst.
+    state = fields.Selection(
+        _INSTALLATION_STATE_VALUES,
+        'Status',
+        help='Where the module has been developed',
+        default='setup'
+    )
+    additional_info = fields.Text('Additional info')
 
-        # Note the misnamed table module_partner_rel!
-        'partner_ids':     fields.many2many('res.partner', 'module_partner_rel', 'installation_id', 'partner_id',
-                                                 string='Customers', help='Customers of this installation.'),
+    platform_id = fields.Many2one('software_knowledge_base.platform', string='Platform')
 
-        
-        'module_ids':     fields.many2many('software_knowledge_base.module', 'module_installation_rel', 'installation_id', 'module_id',
-                                                 string='Modules', help='Modules used by this installation.'),
-                        
-        'external_component_ids': fields.many2many('software_knowledge_base.external_component', 'installation_ext_comp_rel', 'installation_id', 'ext_comp_id',
-                                                 string='External components',help='Libraries and other third party components used by this installation.'),
+    server_id = fields.Many2one('software_knowledge_base.server', string='Server')
+    server_ip_address = fields.Char('Server IP-address', compute='compute_server_ip_address')
 
-        'data_copied_to_m2m': fields.boolean('Partner data moved to new m2m field', groups='base.group_system'),
-    }
-    
-    _defaults = {
-        'state': 'setup',
-    }
+    port = fields.Integer('Port')
+
+    db_server_id = fields.Many2one('software_knowledge_base.server', string='Database server')
+    db_server_ip_address = fields.Char('DB server IP-address', compute='compute_db_server_ip_address')
+
+    disk_usage = fields.Float('Disk Usage (MB)')
+    url = fields.Char('URL')
+    identifier = fields.Char('Identifier')
+
+    project_ids = fields.Many2many(
+        'project.project', 'installation_project_rel', 'installation_id', 'project_id',
+        string='Projects', help='Projects utilizing this installation.'
+    )
+
+    # Note the misnamed table module_partner_rel!
+    partner_ids = fields.Many2many(
+        'res.partner', 'module_partner_rel', 'installation_id', 'partner_id',
+        string='Customers', help='Customers of this installation.'
+    )
+
+    module_ids = fields.Many2many(
+        'software_knowledge_base.module', 'module_installation_rel', 'installation_id', 'module_id',
+        string='Modules', help='Modules used by this installation.'
+    )
+
+    external_component_ids = fields.Many2many(
+        'software_knowledge_base.external_component', 'installation_ext_comp_rel', 'installation_id', 'ext_comp_id',
+        string='External components', help='Libraries and other third party components used by this installation.'
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+    def compute_server_ip_address(self):
+        for record in self:
+            record.server_ip_address = record.server_id.ip_address
+
+    def compute_db_server_ip_address(self):
+        for record in self:
+            record.db_server_ip_address = record.db_server_id.ip_address
+
+            # 5. Constraints and onchanges
+
+            # 6. CRUD methods
+
+            # 7. Action methods
+
+            # 8. Business methods
--- models/server.py
+++ models/server.py
@@ -1,23 +1,47 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import osv, fields
-from openerp.tools.translate import _
 
-class server(osv.Model):
-    
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
+
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class SoftwareKnowledgeBaseServer(models.Model):
+    # 1. Private attributes
     _name = 'software_knowledge_base.server'
-    _description = 'Server'    
-    _inherit = ['mail.thread']    
+    _description = 'Server'
+    _inherit = ['mail.thread']
     _order = 'name'
-    
-    _columns = {
-        'name': fields.char('Name'),
-        'company_id': fields.many2one('res.company', 'Company'),
-        
-        'ip_address': fields.char('IP Address'),
-        'operating_system': fields.char('Operating system'),
-        'installation_ids': fields.one2many('software_knowledge_base.installation', 'server_id', 'Installations'),
-        'note_ids': fields.one2many('software_knowledge_base.server_note', 'server_id', 'Server note'),
-        
-        'specification': fields.text('Technical specification'),
-        'additional_info': fields.text('Additional info'),
-    }
+
+    # 2. Fields declaration
+    name = fields.Char('Name')
+    company_id = fields.Many2one('res.company', 'Company')
+
+    ip_address = fields.Char('IP Address')
+    operating_system = fields.Char('Operating system')
+    installation_ids = fields.One2many('software_knowledge_base.installation', 'server_id', 'Installations')
+    note_ids = fields.One2many('software_knowledge_base.server_note', 'server_id', 'Server note')
+
+    specification = fields.Text('Technical specification')
+    additional_info = fields.Text('Additional info')
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
