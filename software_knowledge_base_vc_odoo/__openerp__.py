1b6828d3646d2650765ec38f97d6d134ebb0b5f4
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-02-17 12:55:11 +0200

    Add a module for Odoo-spesific version control tools

---
+++ software_knowledge_base_vc_odoo/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2016 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': 'SDKB Version control - Odoo',
+    'summary': 'Adds Odoo-spesific tools for Software development knowledge base version control',
+    'version': '8.0.0.1.0',
+    'category': 'Knowledge',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [
+
+        ],
+        'bin': [],
+    },
+    'depends': [
+        'software_knowledge_base_vc',
+    ],
+    'data': [
+
+    ],
+    'demo': [
+    ],
+}
