d467f6d7156195b293c1ad5907732aa6d98fade7
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-11-07 15:24:52 +0200

    Improve installation searches and views

--- software_knowledge_base/__openerp__.py
+++ software_knowledge_base/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Software development knowledge base',
     'summary': 'A module for maintaining data regarding software installations and modules',
-    'version': '8.0.1.0.21',
+    'version': '8.0.1.0.22',
     'category': 'Knowledge',
     'website': 'http://www.vizucom.com',
     'author': 'Vizucom Oy',
--- software_knowledge_base/models/installation.py
+++ software_knowledge_base/models/installation.py
@@ -1,22 +1,8 @@
 # -*- coding: utf-8 -*-
-
-# 1. Standard library imports:
-
-# 2. Known third party imports:
-
-# 3. Odoo imports (openerp):
 from openerp import api, fields, models
 
 
-# 4. Imports from Odoo modules:
-
-# 5. Local imports in the relative form:
-
-# 6. Unknown third party imports:
-
-
 class Installation(models.Model):
-    # 1. Private attributes
     _name = 'software_knowledge_base.installation'
     _description = 'Installation'
     _inherit = ['mail.thread']
@@ -28,7 +14,6 @@ class Installation(models.Model):
         ('terminated', 'Terminated')
     ]
 
-    # 2. Fields declaration
     name = fields.Char('Name')
     company_id = fields.Many2one('res.company', 'Company')
 
@@ -43,12 +28,12 @@ class Installation(models.Model):
     platform_id = fields.Many2one('software_knowledge_base.platform', string='Platform')
 
     server_id = fields.Many2one('software_knowledge_base.server', string='Server')
-    server_ip_address = fields.Char('Server IP-address', compute='compute_server_ip_address')
+    server_ip_address = fields.Char('Server IP-address', related='server_id.ip_address')
 
     port = fields.Integer('Port')
 
     db_server_id = fields.Many2one('software_knowledge_base.server', string='Database server')
-    db_server_ip_address = fields.Char('DB server IP-address', compute='compute_db_server_ip_address')
+    db_server_ip_address = fields.Char('DB server IP-address', related='db_server_id.ip_address')
 
     disk_usage = fields.Float('Disk Usage (MB)')
     url = fields.Char('URL')
@@ -74,22 +59,3 @@ class Installation(models.Model):
         'software_knowledge_base.external_component', 'installation_ext_comp_rel', 'installation_id', 'ext_comp_id',
         string='External components', help='Libraries and other third party components used by this installation.'
     )
-
-    # 3. Default methods
-
-    # 4. Compute and search fields, in the same order that fields declaration
-    def compute_server_ip_address(self):
-        for record in self:
-            record.server_ip_address = record.server_id.ip_address
-
-    def compute_db_server_ip_address(self):
-        for record in self:
-            record.db_server_ip_address = record.db_server_id.ip_address
-
-            # 5. Constraints and onchanges
-
-            # 6. CRUD methods
-
-            # 7. Action methods
-
-            # 8. Business methods
