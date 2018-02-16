a7ce85808e932f806ec66d5416005dd3eb627d38
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-02-16 23:01:33 +0200

    Add readme parsing

--- software_knowledge_base_vc_odoo/__openerp__.py
+++ software_knowledge_base_vc_odoo/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'SDKB Version control - Odoo',
     'summary': 'Adds Odoo-spesific tools for Software development knowledge base version control',
-    'version': '8.0.0.2.0',
+    'version': '8.0.0.3.0',
     'category': 'Knowledge',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
--- software_knowledge_base_vc_odoo/models/swkb_repository.py
+++ software_knowledge_base_vc_odoo/models/swkb_repository.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # 1. Standard library imports:
+import base64
+import re
 
 # 2. Known third party imports:
-import gitlab
 
 # 3. Odoo imports (openerp):
 from openerp import api, fields, models
@@ -32,7 +33,7 @@ class SWKBRepository(models.Model):
     # 7. Action methods
     @api.multi
     def action_update_odoo_modules(self):
-        module_model = self.env['software_knowledge_base.module']
+        SWKBModule = self.env['software_knowledge_base.module']
 
         for record in self:
             files = record.api_gitlab_get_files()
@@ -46,22 +47,50 @@ class SWKBRepository(models.Model):
                     sub_files = record.api_gitlab_get_files(path=file['path'])
 
                     readme = record.api_get_readme_from_repository_tree(sub_files)
+                    manifest = record.api_get_manifest_from_repository_tree(sub_files)
+                    summary = ''
 
-                    if readme:
-                        if module_model.search([('name', '=', file['name'])]):
-                            # Update module
-                            module_model.write({
-                                'readme': readme,
-                                'repository': record.id,
-                            })
-                        else:
-                            # Create a new module
-                            module_model.create({
-                                'name': file['name'],
-                                'readme': readme,
-                                'repository': record.id,
-                            })
+                    if manifest:
+                        pattern = "['\"](name|summary|version)[\"'][:][ ]['\"](.*)['\"]"
+                        p = re.compile(pattern)
 
+                        result = dict(p.findall(manifest))
+
+                        if 'summary' in result:
+                            summary = result['summary']
+
+                    existing_module = SWKBModule.search([
+                        ('name', '=', file['name']),
+                        ('repository', '=', record.id),
+                    ])
+
+                    if existing_module:
+                        # Update module
+                        existing_module.write({
+                            'readme': readme,
+                            'repository': record.id,
+                            'summary': summary,
+                        })
+                    else:
+                        # Create a new module
+                        SWKBModule.create({
+                            'name': file['name'],
+                            'readme': readme,
+                            'repository': record.id,
+                            'summary': summary,
+                        })
 
     # 8. Business methods
+    def api_get_manifest_from_repository_tree(self, tree):
+        if not tree:
+            return False
 
+        for file in tree:
+            if file['name'][0:15].lower() == '__manifest__.py' or \
+                file['name'][0:14] == '__openerp__.py':
+                manifest_file = self.api_gitlab_get_file(file['path'])
+                manifest_file_content = base64.b64decode(manifest_file['content'])
+
+                return manifest_file_content
+
+        return False
