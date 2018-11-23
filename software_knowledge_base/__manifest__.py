e95de1ef7a3cf20e9ae25c76dd7471e7ea00c80c
Timo Talvitie <timo.talvitie@tawasta.fi>
2018-11-23 22:53:13 +0200

    Update finnish i18n

--- software_knowledge_base/__manifest__.py
+++ software_knowledge_base/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Software development knowledge base',
     'summary': 'Maintain software installation and module data',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'Specific Industry Applications',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
