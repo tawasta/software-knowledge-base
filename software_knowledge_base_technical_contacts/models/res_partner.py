554ebb4567f1e9fb97f0afc499c3f1c6df2f1359
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2020-01-03 10:27:03 +0200

    [ADD] Field and filter for partner servers

--- software_knowledge_base_technical_contacts/models/res_partner.py
+++ software_knowledge_base_technical_contacts/models/res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
@@ -15,3 +15,14 @@ class ResPartner(models.Model):
         help='Installations where the partner has been marked as a '
              'technical contact.'
     )
+
+    server_technical_contact_ids = fields.Many2many(
+        comodel_name='software_knowledge_base.server',
+        string='Servers (Technical contact)',
+        compute='_compute_server_technical_contact_ids',
+    )
+
+    def _compute_server_technical_contact_ids(self):
+        for record in self:
+            record.server_technical_contact_ids = \
+                record.installation_technical_contact_ids.mapped('server_id')
