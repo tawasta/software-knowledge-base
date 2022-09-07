31859135547ec273b1b72a3710298248031477c0
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-09-07 10:07:42 +0300

    [FIX] Add a missing description

--- software_knowledge_base_vcs/models/vcs_api.py
+++ software_knowledge_base_vcs/models/vcs_api.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 
 class SWKBVcsApi(models.Model):
     _name = "software_knowledge_base.vcs_api"
+    _description = "VCS API"
 
     name = fields.Char("VCS API name", readonly=True)
     code = fields.Char("VCS API code", readonly=True)
